dab8bf0ea069c7869a67a892a5c368bab6b6842e
Casey Yang <knightykc@gmail.com>
2014-11-08 22:19:36 +0800

    update;

---
+++ LeetCode/Palindrome_Partitioning/Main.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+	void deepSearch(int **dp, vector<vector<string>> &result, vector<string> &oneResult, string s, int start){
+		if (start == s.size()){
+			vector<string> tmp = vector<string>();
+			for (int i = 0; i < oneResult.size(); ++i){
+				tmp.push_back(oneResult[i]);
+			}
+			result.push_back(tmp);
+			return;
+		}
+		else{
+			for (int i = start; i < s.size(); ++i){
+				if (dp[start][i] == 1){
+					oneResult.push_back(s.substr(start, i - start + 1));
+					deepSearch(dp, result, oneResult, s, i + 1);
+					oneResult.pop_back();
+				}
+			}
+			return;
+		}
+	}
+	vector<vector<string>> partition(string s) {
+		vector<vector<string>> result = vector<vector<string>>();
+		if (s.size() == 0){
+			return result;
+		}
+		else{
+			int **dp = new int*[s.size()];
+			for (int i = 0; i < s.size(); ++i){
+				dp[i] = new int[i];
+				for (int j = 0; j < s.size(); ++j){
+					dp[i][j] = 0;
+				}
+				for (int j = i; j < s.size(); ++j){
+					int k = 0;
+					while (i + k <= j - k&&s[i + k] == s[j - k]){
+						++k;
+					}
+					if (i + k>(i + j) / 2){
+						dp[i][j] = 1;
+					}
+				}
+			}
+			vector<string> oneResult = vector<string>();
+			deepSearch(dp, result, oneResult, s, 0);
+			delete[]dp;
+			return result;
+		}
+	}
+};
+
+int main(){
+	string ss = "abbab";
+	Solution s;
+	vector<vector<string>> result = s.partition(ss);
+	for (int i = 0; i < result.size(); ++i){
+		for (int j = 0; j < result[i].size(); ++j){
+			cout << result[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
--- LeetCode/Palindrome_Partitioning_II/Main.cpp
+++ LeetCode/Palindrome_Partitioning_II/Main.cpp
@@ -1,1 +1,75 @@
 #include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+void OutputD(int *D,int l){
+	for (int i = 0; i < l; ++i){
+		cout << D[i] << " ";
+	}
+	cout << endl;
+}
+
+void OutputP(bool **P, int l){
+	for (int i = 0; i < l; ++i){
+		for (int j = 0; j < l; ++j){
+			cout << P[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+class Solution {
+public:
+	int minCut(string s) {
+		int len = s.size();
+		int *D = new int[len + 1];
+		bool **P = new bool*[len];
+		for (int i = 0; i < len; ++i){
+			P[i] = new bool[len];
+		}
+		for (int i = 0; i <= len; i++){
+			D[i] = len - i;
+		}
+		for (int i = 0; i < len; i++){
+			for (int j = 0; j < len; j++){
+				P[i][j] = false;
+			}
+		}
+		for (int i = len - 1; i >= 0; i--){
+			for (int j = i; j < len; j++){
+				if (s[i] == s[j] && (j - i < 2 || P[i + 1][j - 1])){
+					P[i][j] = true;
+					D[i] = min(D[i], D[j + 1] + 1);
+				}
+			}
+		}
+		OutputD(D, len + 1);
+		cout << endl;
+		OutputP(P, len);
+		return D[0] - 1;
+	}
+
+	int minCut2(string s) {
+		int n = s.size();
+		vector<int> cut(n + 1, 0);  // number of cuts for the first k characters
+		for (int i = 0; i <= n; i++) cut[i] = i - 1;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; i - j >= 0 && i + j < n && s[i - j] == s[i + j]; j++) // odd length palindrome
+				cut[i + j + 1] = min(cut[i + j + 1], 1 + cut[i - j]);
+
+			for (int j = 1; i - j + 1 >= 0 && i + j < n && s[i - j + 1] == s[i + j]; j++) // even length palindrome
+				cut[i + j + 1] = min(cut[i + j + 1], 1 + cut[i - j + 1]);
+		}
+		return cut[n];
+	}
+};
+
+int main(){
+	string ss = "abbacabbacd";
+	Solution s;
+	s.minCut(ss);
+	return 0;
+}

8bf91b523a8b19f31cc0509d88cfdd11780ad085
Casey Yang <knightykc@gmail.com>
2014-11-10 00:07:27 +0800

    update;

--- LeetCode/Best_Time_to_Buy_and_Sell_Stock_III/Main.cpp
+++ LeetCode/Best_Time_to_Buy_and_Sell_Stock_III/Main.cpp
@@ -10,13 +10,31 @@ public:
 			return 0;
 		}
 		else{
-			int **dp = new int*[prices.size()];
-			for (int i = 0; i < prices.size(); ++i){
-				dp[i] = new int[prices.size()];
-				for (int j = 0; j < prices.size(); ++j){
-					dp[i][j] = prices[j] - prices[i];
+			int *sellDays = new int[prices.size()];
+			sellDays[0] = 0;
+			int *buyDays = new int[prices.size()];
+			buyDays[prices.size() - 1] = 0;
+			int *dp = new int[prices.size()];
+			dp[0] = 0;
+			for (int sellDay = 1; sellDay < prices.size(); ++sellDay){
+				sellDays[sellDay] = 0;
+				for (int i = 1; i <= sellDay; i++){
+					dp[i] = max(dp[i - 1] + prices[i] - prices[i - 1], 0);
+					sellDays[sellDay] = dp[i] > sellDays[sellDay] ? dp[i] : sellDays[sellDay];
 				}
-			}			
+			}
+			for (int buyDay = prices.size() - 1; buyDay >= 0; --buyDay){
+				for (int i = prices.size() - 1; i >= buyDay; --i){
+					dp[i] = max(dp[i + 1] + (prices[i + 1] - prices[i]), 0);
+					buyDays[buyDay] = dp[i] > buyDays[buyDay] ? dp[i] : buyDays[buyDay];
+				}
+			}
+			int totalMax = 0;
+			for (int i = 0; i < prices.size(); ++i){
+				int cur = buyDays[i] + sellDays[i];
+				totalMax = totalMax>cur ? totalMax : cur;
+			}
+			return;
 		}
 	}
 };

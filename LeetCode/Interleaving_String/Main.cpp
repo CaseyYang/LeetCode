e275424fe718b247bbbdace50ffcd6f9b3de88ff
Casey Yang <knightykc@gmail.com>
2014-11-13 09:45:08 +0800

    update;

--- LeetCode/Interleaving_String/Main.cpp
+++ LeetCode/Interleaving_String/Main.cpp
@@ -12,35 +12,29 @@ public:
 			if (s3 == "") return true;
 			if (s1 == "") return s2 == s3;
 			if (s2 == "") return s1 == s3;
-			bool **dp = new bool*[s1.size()];
-			for (int i = 0; i < s1.size(); ++i){
-				dp[i] = new bool[s2.size()];
-				for (int j = 0; j < s2.size(); ++j){
-					if (i == 0){
-						if (j == 0){
-							dp[0][0] = s3[1] == s2[0] && s3[0] == s1[0] || s3[1] == s1[0] && s3[0] == s2[0];
-						}
-						else{
-							dp[i][j] = dp[i][j - 1] & (s3[i + j + 1] == s2[j]);
-						}
-					}
-					else{
-						if (j == 0){
-							dp[i][j] = dp[i - 1][0] & (s1[i] == s3[i + j + 1]);
-						}
-						else{
-							dp[i][j] = dp[i - 1][j] & (s1[i] == s3[i + j + 1]) || dp[i][j - 1] & (s1[j] == s3[i + j + 1]);
-						}
-					}
+			bool **dp = new bool*[s1.size() + 1];
+			for (int i = 0; i <= s1.size(); ++i){
+				dp[i] = new bool[s2.size() + 1];
+			}
+			dp[0][0] = true;
+			for (int i = 1; i <= s1.size(); ++i){
+				dp[i][0] = dp[i - 1][0] && s1[i - 1] == s3[i - 1];
+			}
+			for (int j = 1; j <= s2.size(); ++j){
+				dp[0][j] = dp[0][j - 1] && s2[j - 1] == s3[j - 1];
+			}
+			for (int i = 1; i <= s1.size(); ++i){
+				for (int j = 1; j <= s2.size(); ++j){
+					dp[i][j] = dp[i - 1][j] && s1[i - 1] == s3[i + j - 1] || dp[i][j - 1] && s2[j - 1] == s3[i + j - 1];
 				}
 			}
-			return dp[s1.size() - 1][s2.size() - 1];
+			return dp[s1.size()][s2.size()];
 		}
 	}
 };
 
 int main(){
-	string s1 = "aa", s2 = "ab", s3 = "aaba";
+	string s1 = "aa", s2 = "ba", s3 = "aaba";
 	Solution s;
 	cout << s.isInterleave(s1, s2, s3) << endl;
 	return 0;

b4cfea8bb088233fc61823410dd66438876287c3
Casey Yang <knightykc@gmail.com>
2014-11-15 22:33:10 +0800

    update;

---
+++ LeetCode/Partition_List/Main.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution {
+public:
+	ListNode *partition(ListNode *head, int x) {
+		if (head == NULL || head->next == NULL) return head;
+		ListNode *l1Head = NULL;
+		ListNode *l1Cur = NULL;
+		ListNode *l2Head = NULL;
+		ListNode *l2Cur = NULL;
+		ListNode *cur = head;
+		while (cur != NULL){
+			cout << cur->val << endl;
+			if (cur->val < x){
+				if (l1Head == NULL) {
+					l1Head = cur;
+					l1Cur = cur;
+				}
+				else{
+					l1Cur->next = cur;
+					l1Cur = l1Cur->next;
+				}
+			}
+			else{
+				if (l2Head == NULL){
+					l2Head = cur;
+					l2Cur = cur;
+				}
+				else{
+					l2Cur->next = cur;
+					l2Cur = l2Cur->next;
+				}
+				cout << l2Cur->val << " " << l2Head->val << endl;
+			}
+			cur = cur->next;
+		}
+		if (l2Head != NULL) l2Cur->next = NULL;
+		if (l1Head == NULL) return l2Head;
+		l1Cur->next = l2Head;
+		return l1Head;
+	}
+};
+int main(){
+	ListNode *l1 = new ListNode(1);
+	ListNode *l2 = new ListNode(2);
+	l1->next = l2;
+	Solution s;
+	s.partition(l1, 0);
+}

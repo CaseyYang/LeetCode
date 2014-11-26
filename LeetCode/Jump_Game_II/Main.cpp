5834920d6e2ee17ce5ab72ad737dadb247058b4a
Casey Yang <knightykc@gmail.com>
2014-11-26 21:52:17 +0800

    update;

--- LeetCode/Jump_Game_II/Main.cpp
+++ LeetCode/Jump_Game_II/Main.cpp
@@ -4,16 +4,8 @@ using namespace std;
 class Solution {
 public:
 	int jump(int A[], int n) {
-		vector<int> v = vector<int>(n);
-		for (int i = 0; i < n; ++i) v[i] = 0;
-		if (n <= 1) return 1;
-		int i = 0;
-		int farest = 0;
-		while (i <= farest){
-			if (farest == n - 1) return v[farest];
-			farest = i + A[i];
-			v[farest] = v[i] + 1;
-		}
+		
+		
 	}
 };
 int main(){

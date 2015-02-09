4da45ddcd8a8053ad881fe25dbc3244ebe3233b4
Casey Yang <knightykc@gmail.com>
2015-02-09 23:49:58 +0800

    Finish Problem Fraction to Recurring Decimal;

--- LeetCode/Fraction_to_Recurring_Decimal/Main.cpp
+++ LeetCode/Fraction_to_Recurring_Decimal/Main.cpp
@@ -1,23 +1,51 @@
 #include<iostream>
 #include<sstream>
+#include<map>
 using namespace std;
 class Solution {
 public:
 	string fractionToDecimal(int numerator, int denominator) {
-		string result;
-		long longNumerator = numerator;
-		long longDenominator = denominator;
-		int answer = longNumerator / longDenominator;
+		if (numerator == 0) return "0";
+		if (denominator == 0) return "";
+		string result, tmpStr;
+		if (numerator > 0 && denominator < 0 || numerator < 0 && denominator>0) result = "-";
+		long long longNumerator = numerator;//一定要long long。用long在遇到-2147483648还是会出错
+		long long longDenominator = denominator;
+		longNumerator = abs(longNumerator);
+		longDenominator = abs(longDenominator);
+		long long answer = longNumerator / longDenominator;
 		stringstream ss;
 		ss << answer;
-		ss >> result;
-		int remainder = longNumerator / longDenominator;
+		ss >> tmpStr;
+		ss.clear();
+		result += tmpStr;
+		long long remainder = longNumerator % longDenominator;
 		if (remainder != 0){
+			cout << remainder << endl;
+			map<long long, int> remainders;
 			result += ".";
-			longNumerator = remainder;
+			remainder = remainder * 10;
+			while (remainder != 0){
+				if (remainders.find(remainder) != remainders.end()){
+					string p1 = result.substr(0, remainders[remainder]);
+					string p2 = result.substr(remainders[remainder]);
+					result = p1 + "(" + p2 + ")";
+					break;
+				}
+				remainders[remainder] = result.size();
+				answer = remainder / longDenominator;
+				ss << answer;
+				ss >> tmpStr;
+				ss.clear();
+				result += tmpStr;
+				remainder = (remainder % longDenominator) * 10;
+			}
 		}
+		return result;
 	}
 };
 int main(){
+	Solution s;
+	cout << s.fractionToDecimal(-1, -2147483648) << endl;
 	return 0;
 }

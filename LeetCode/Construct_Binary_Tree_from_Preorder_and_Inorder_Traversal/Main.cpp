918c71e121e2d3861377b18124b4dad9c5002e48
Casey Yang <knightykc@gmail.com>
2015-02-18 12:49:22 +0800

    update;

--- LeetCode/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/Main.cpp
+++ LeetCode/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/Main.cpp
@@ -13,32 +13,28 @@ struct TreeNode {
 class Solution {
 public:
 	TreeNode *recursion(vector<int> &preorder, vector<int>::iterator startIter, vector<int>::iterator endIter, vector<int> &inorder, int &index){
-		if (startIter == endIter || index >= preorder.size()){
-			return NULL;
+		if (startIter == endIter || index >= preorder.size()){ return NULL; }
+		if (startIter + 1 == endIter){
+			TreeNode *newNode = new TreeNode(preorder[index]);
+			++index;
+			return newNode;
 		}
 		else{
-			if (startIter + 1 == endIter){
-				TreeNode *newNode = new TreeNode(preorder[index]);
-				++index;
-				return newNode;
-			}
-			else{
-				TreeNode *newNode = new TreeNode(preorder[index]);
-				vector<int>::iterator curIter = find(inorder.begin(), inorder.end(), newNode->val);
-				++index;
-				if (index < preorder.size()){
-					vector<int>::iterator nextIter = find(inorder.begin(), inorder.end(), preorder[index]);
-					if (nextIter < curIter){
-						newNode->left = recursion(preorder, startIter, curIter, inorder, index);
-						newNode->right = recursion(preorder, curIter + 1, endIter, inorder, index);
-					}
-					else{
-						newNode->right = recursion(preorder, curIter + 1, endIter, inorder, index);
-						newNode->left = recursion(preorder, startIter, curIter, inorder, index);
-					}
+			TreeNode *newNode = new TreeNode(preorder[index]);
+			vector<int>::iterator curIter = find(inorder.begin(), inorder.end(), newNode->val);
+			++index;
+			if (index < preorder.size()){
+				vector<int>::iterator nextIter = find(inorder.begin(), inorder.end(), preorder[index]);
+				if (nextIter < curIter){
+					newNode->left = recursion(preorder, startIter, curIter, inorder, index);
+					newNode->right = recursion(preorder, curIter + 1, endIter, inorder, index);
 				}
-				return newNode;
+				else{
+					newNode->right = recursion(preorder, curIter + 1, endIter, inorder, index);
+					newNode->left = recursion(preorder, startIter, curIter, inorder, index);
+				}
 			}
+			return newNode;
 		}
 	}
 	TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {

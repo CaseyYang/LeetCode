01e2bc96e78146d92647d98431536bfefbf3a590
Casey Yang <knightykc@gmail.com>
2015-09-24 16:13:49 +0800

    update;

--- LeetCode/H-Index_II/Main.cpp
+++ LeetCode/H-Index_II/Main.cpp
@@ -8,7 +8,6 @@ class Solution {
 public:
 	int hIndex(vector<int>& citations) {
 		if (citations.size() == 0) return 0;
-		sort(citations.begin(), citations.end());
 		int current = 0;
 		for (int i = citations.size() - 1; i >= 0; --i){
 			if (citations[i] >= citations.size() - i){

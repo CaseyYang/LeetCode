c050e6247d27e6da0f36f633ed60bc5b2dd53f32
Casey Yang <knightykc@gmail.com>
2015-10-06 15:50:18 +0800

    update;

--- LeetCode/Implement_Queue_using_Stacks/Main.cpp
+++ LeetCode/Implement_Queue_using_Stacks/Main.cpp
@@ -8,21 +8,78 @@ public:
 	stack<int> s2;
 	// Push element x to the back of queue.
 	void push(int x) {
-
+		if (!s1.empty()){
+			s1.push(x);
+		}
+		else{
+			s2.push(x);
+		}
 	}
 
 	// Removes the element from in front of queue.
 	void pop(void) {
-
+		if (!s1.empty()){
+			while (!s1.empty()){
+				int tmp = s1.top();
+				s1.pop();
+				s2.push(tmp);
+			}
+			s2.pop();
+			while (!s2.empty()){
+				int tmp = s2.top();
+				s2.pop();
+				s1.push(tmp);
+			}
+		}
+		else{
+			while (!s2.empty()){
+				int tmp = s2.top();
+				s2.pop();
+				s1.push(tmp);
+			}
+			s1.pop();
+			while (!s1.empty()){
+				int tmp = s1.top();
+				s1.pop();
+				s2.push(tmp);
+			}
+		}
 	}
 
 	// Get the front element.
 	int peek(void) {
-
+		int result = -1;
+		if (!s1.empty()){
+			while (!s1.empty()){
+				int tmp = s1.top();
+				s1.pop();
+				s2.push(tmp);
+			}
+			result = s2.top();
+			while (!s2.empty()){
+				int tmp = s2.top();
+				s2.pop();
+				s1.push(tmp);
+			}
+		}
+		else{
+			while (!s2.empty()){
+				int tmp = s2.top();
+				s2.pop();
+				s1.push(tmp);
+			}
+			result = s1.top();
+			while (!s1.empty()){
+				int tmp = s1.top();
+				s1.pop();
+				s2.push(tmp);
+			}
+		}
+		return result;
 	}
 
 	// Return whether the queue is empty.
 	bool empty(void) {
-
+		return s1.empty() && s2.empty();
 	}
 };

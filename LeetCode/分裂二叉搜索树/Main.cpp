2aa19f8b2a34539d4174931b1dae4473baa74f1f
Casey Yang <knightykc@gmail.com>
2015-10-18 15:53:31 +0800

    update;

---
+++ LeetCode/分裂二叉搜索树/Main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct TreeNode {
+	int value;
+	TreeNode* left;
+	TreeNode* right;
+};
+
+vector<TreeNode*> func(TreeNode* root,int n) {
+	TreeNode* cur = root;
+	TreeNode* smaller = NULL, *bigger = NULL;
+	while(cur!=NULL&&)
+}
+
+int main() {
+	return 0;
+}
